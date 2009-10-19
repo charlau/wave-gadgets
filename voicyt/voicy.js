82aaaf0496701bbeb940b7dfcb20b3a93a88a6d1
charlesmtl <charlesmtl@1e5c4b6a-bb6b-11de-bb2d-0d8fa6df8a7d>
2009-10-19 09:18:06 +0000

    added voicyt test branch

---
+++ voicyt/voicy.js
@@ -0,0 +1,78 @@
+
+	var msg;
+	var loadMessage;
+	var wooYayIntervalId = 0;
+	var prefs = new gadgets.Prefs();
+	var debugMode = prefs.getBool("debugMode");
+	var firstrun = prefs.getBool("firstrun");
+	var firstpass = true;
+	var theBasePath;
+	var myID;
+	var theHost;
+	var iCanListen = false;
+	var tabs;
+	var iframeWin;
+	var myRamdom="";
+	var firstStatus = true;
+	var waitingForCharlau = true;
+	var gagheight;
+	var Connected = false;
+	var iframeSrc = "";
+	var iamTheHost = false;
+	var nbmessages;
+	var nbmessnew;
+	var IamRecording = false;
+	var particiPready = false;
+
+	
+	function init(){
+			msg = new gadgets.MiniMessage();
+			loadMessage = msg.createStaticMessage("loading gadget");
+			gagheight = "210";
+			if (prefs.getString("zfile") == "") {
+				prefs.set("zfile", randomString(15)+".txt"); 
+			}
+			prefs.set("firstrun",false);
+			wave.setStateCallback(stateUpdated);
+	}
+
+	function stateUpdated() {
+		iframeWin = document.getElementsByTagName('iframe')[0].contentWindow;
+		window.addEventListener('message', receiver, false);
+		iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
+		msg.createDismissibleMessage("***I ping***");
+	}
+
+	function receiver(e) {
+		msg.dismissMessage(loadMessage);
+		msg.createDismissibleMessage("***received***" + e.origin + "***" + e.source);
+		if(e.origin == 'http://www.charlau.com') {
+			var messages = e.data.split("~~om~~");
+				switch (messages[0]){
+				case "[ping]":
+					msg.createDismissibleMessage("***YOU ping***" + e.origin + "***" + e.source+"yourorigin:"+messages[1]+"yoursource:"+messages[2]);
+					break;
+				default:
+				}
+			}	
+		}
+
+	function randomString(length){
+		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		var rSt = "";
+		var i;
+		for(x=0;x<length;x++){
+			i = Math.floor(Math.random() * 62);
+			rSt += chars.charAt(i);
+	  	}
+		return rSt;
+	}
+
+	function loGit(tolog){
+		if (debugMode) {
+			msg.createDismissibleMessage("*** " + tolog.toString() + " ***");
+		}
+	}
+
+    gadgets.util.registerOnLoadHandler(init);
+    gadgets.window.adjustHeight();
---
+++ voicyt/voicyt.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Record and Play audio greetings in google wave</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+
+ <script type="text/javascript">
+ 
+	var parentWin = window.parent;
+	var listCount = 0;
+	var thesource = "";
+	
+	window.onload = function(){ 
+            window.addEventListener("message", function(e){ 
+				if (e.origin == 'https://0-wave-opensocial.googleusercontent.com') {
+					thesource = e.origin;
+					var messages = e.data.split("~~om~~");
+					switch (messages[0]){
+					case "[ping]":
+						alert("ping");
+						parentWin.postMessage("[ping]~~om~~"+e.origin+"~~om~~"+e.source+"~~om~~",e.origin);
+						break;
+					default:
+				}
+			}, false);			 
+		} 
+
+  </script>
+
+</head>
+
+<body>
+</body>
+</html>
