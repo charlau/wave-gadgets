320682dbb824ed8285743c6ccb5db47da6351edd
charlesmtl <charlesmtl@1e5c4b6a-bb6b-11de-bb2d-0d8fa6df8a7d>
2009-10-19 09:29:42 +0000

    asdf

--- trunk/voicyt/voicyt.php
+++ trunk/voicyt/voicyt.php
@@ -19,13 +19,22 @@
 					var messages = e.data.split("~~om~~");
 					switch (messages[0]){
 					case "[ping]":
+						clearInterval(wooYayIntervalId);
 						alert("ping");
 						parentWin.postMessage("[ping]~~om~~"+e.origin+"~~om~~"+e.source+"~~om~~",e.origin);
 						break;
 					default:
+					}
 				}
 			}, false);			 
 		} 
+
+	function pinggoogle(){
+		
+		parentWin.postMessage("[ping]~~om~~1~~om~~2~~om~~",'https://0-wave-opensocial.googleusercontent.com');
+		
+		}
+		var xy = setInterval("pinggoogle()", 1000);
 
   </script>
 
