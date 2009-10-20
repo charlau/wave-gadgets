0e6c7c634621e090ecb2a99c93dcd0707a33658a
charlesmtl <charlesmtl@1e5c4b6a-bb6b-11de-bb2d-0d8fa6df8a7d>
2009-10-20 00:19:44 +0000

    dfg

--- voicyt/voicy.js
+++ voicyt/voicy.js
@@ -1,63 +1,29 @@
 
-	var msg;
-	var loadMessage;
-	var wooYayIntervalId = 0;
-	var prefs = new gadgets.Prefs();
-	var debugMode = prefs.getBool("debugMode");
-	var firstrun = prefs.getBool("firstrun");
-	var firstpass = true;
-	var theBasePath;
-	var myID;
-	var theHost;
-	var iCanListen = false;
-	var tabs;
-	var iframeWin;
-	var myRamdom="";
-	var firstStatus = true;
-	var waitingForCharlau = true;
-	var gagheight;
-	var Connected = false;
-	var iframeSrc = "";
-	var iamTheHost = false;
-	var nbmessages;
-	var nbmessnew;
-	var IamRecording = false;
-	var particiPready = false;
-
+var msg;
+var loadMessage;
+var wooYayIntervalId = 0;
+var prefs = new gadgets.Prefs();
+var debugMode = prefs.getBool("debugMode");
+var firstrun = prefs.getBool("firstrun");
+var firstpass = true;
+var theBasePath;
+var myID;
+var theHost;
+var iCanListen = false;
+var tabs;
+var iframeWin;
+var myRamdom="";
+var firstStatus = true;
+var waitingForCharlau = true;
+var gagheight;
+var Connected = false;
+var iframeSrc = "";
+var iamTheHost = false;
+var nbmessages;
+var nbmessnew;
+var IamRecording = false;
+var particiPready = false;
 	
-	function init(){
-			msg = new gadgets.MiniMessage();
-			loadMessage = msg.createStaticMessage("loading gadget");
-			gagheight = "210";
-			if (prefs.getString("zfile") == "") {
-				prefs.set("zfile", randomString(15)+".txt"); 
-			}
-			prefs.set("firstrun",false);
-			wave.setStateCallback(stateUpdated);
-	}
-
-	function stateUpdated() {
-//		iframeWin = document.getElementsByTagName('iframe')[0].contentWindow;
-//		window.addEventListener('message', receiver, false);
-//		iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
-//		msg.createDismissibleMessage("***I ping***");
-	}
-
-	function receiver(e) {
-		msg.dismissMessage(loadMessage);
-//		msg.createDismissibleMessage("***received***" + e.origin + "***" + e.source);
-		if(e.origin == 'http://www.charlau.com') {
-			var messages = e.data.split("~~om~~");
-				switch (messages[0]){
-				case "[ping]":
-					msg.createDismissibleMessage("***YOU ping***" + e.origin + "***" + e.source+"yourorigin:"+messages[1]+"yoursource:"+messages[2]);
-					iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
-					break;
-				default:
-				}
-			}	
-		}
-
 var vmessage = function () {
 	try{
 		// Opera 8.0+, Firefox, Safari
@@ -87,8 +53,43 @@ var vmessage = function () {
 
 }
 
-var getList = new vmessage;
-getList.get("http://www.charlau.com/gwave/voicyaj.php?fl=qoweu07qwkncbao.txt");
+
+
+	function init(){
+			msg = new gadgets.MiniMessage();
+			loadMessage = msg.createStaticMessage("loading gadget");
+			gagheight = "210";
+			if (prefs.getString("zfile") == "") {
+				prefs.set("zfile", randomString(15)+".txt"); 
+			}
+			prefs.set("firstrun",false);
+			var getList = new vmessage;
+			getList.get("http://www.charlau.com/gwave/voicyaj.php?fl=" + prefs.getString("zfile"));
+
+			wave.setStateCallback(stateUpdated);
+	}
+
+	function stateUpdated() {
+//		iframeWin = document.getElementsByTagName('iframe')[0].contentWindow;
+//		window.addEventListener('message', receiver, false);
+//		iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
+//		msg.createDismissibleMessage("***I ping***");
+	}
+
+	function receiver(e) {
+		msg.dismissMessage(loadMessage);
+//		msg.createDismissibleMessage("***received***" + e.origin + "***" + e.source);
+		if(e.origin == 'http://www.charlau.com') {
+			var messages = e.data.split("~~om~~");
+				switch (messages[0]){
+				case "[ping]":
+					msg.createDismissibleMessage("***YOU ping***" + e.origin + "***" + e.source+"yourorigin:"+messages[1]+"yoursource:"+messages[2]);
+					iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
+					break;
+				default:
+				}
+			}	
+		}
 
 	function randomString(length){
 		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -106,6 +107,8 @@ getList.get("http://www.charlau.com/gwave/voicyaj.php?fl=qoweu07qwkncbao.txt");
 			msg.createDismissibleMessage("*** " + tolog.toString() + " ***");
 		}
 	}
+
+	
 
     gadgets.util.registerOnLoadHandler(init);
     gadgets.window.adjustHeight();
