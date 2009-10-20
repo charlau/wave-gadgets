41abfe68b0861551b6d851dc98b92ea6ff37e422
charlesmtl <charlesmtl@1e5c4b6a-bb6b-11de-bb2d-0d8fa6df8a7d>
2009-10-20 00:51:57 +0000

    asd

--- voicyt/voicy.js
+++ voicyt/voicy.js
@@ -1,23 +1,14 @@
 
 var msg;
 var loadMessage;
-var wooYayIntervalId = 0;
 var prefs = new gadgets.Prefs();
 var debugMode = prefs.getBool("debugMode");
 var firstrun = prefs.getBool("firstrun");
 var firstpass = true;
-var theBasePath;
 var myID;
 var theHost;
 var iCanListen = false;
 var tabs;
-var iframeWin;
-var myRamdom="";
-var firstStatus = true;
-var waitingForCharlau = true;
-var gagheight;
-var Connected = false;
-var iframeSrc = "";
 var iamTheHost = false;
 var nbmessages;
 var nbmessnew;
@@ -68,27 +59,10 @@ var vmessage = function () {
 
 	function stateUpdated() {
 			var getList = new vmessage;
-			getList.get("http://www.charlau.com/gwave/voicyaj.php");
-//		iframeWin = document.getElementsByTagName('iframe')[0].contentWindow;
-//		window.addEventListener('message', receiver, false);
-//		iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
-//		msg.createDismissibleMessage("***I ping***");
+			getList.open("GET", "http://www.charlau.com/gwave/voicyaj.php&fl=sfdsf.txt", true);
+			getList.send(null);
+//			getList.get("http://www.charlau.com/gwave/voicyaj.php&fl=sfdsf.txt");
 	}
-
-	function receiver(e) {
-		msg.dismissMessage(loadMessage);
-//		msg.createDismissibleMessage("***received***" + e.origin + "***" + e.source);
-		if(e.origin == 'http://www.charlau.com') {
-			var messages = e.data.split("~~om~~");
-				switch (messages[0]){
-				case "[ping]":
-					msg.createDismissibleMessage("***YOU ping***" + e.origin + "***" + e.source+"yourorigin:"+messages[1]+"yoursource:"+messages[2]);
-					iframeWin.postMessage('[ping]~~om~~', 'http://www.charlau.com');
-					break;
-				default:
-				}
-			}	
-		}
 
 	function randomString(length){
 		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -106,8 +80,6 @@ var vmessage = function () {
 			msg.createDismissibleMessage("*** " + tolog.toString() + " ***");
 		}
 	}
-
-	
 
     gadgets.util.registerOnLoadHandler(init);
     gadgets.window.adjustHeight();
