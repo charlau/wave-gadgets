0d1c1579dc10ab466782c8edd3e4fe3236b9f0f0
charlesmtl <charlesmtl@1e5c4b6a-bb6b-11de-bb2d-0d8fa6df8a7d>
2010-01-09 14:32:16 +0000

    added loop wait-wave as it seems might have regressed with "require wave-preview"

--- trunk/voicyt/voicy.js
+++ trunk/voicyt/voicy.js
@@ -18,9 +18,10 @@ var waitingForCharlau = true;
 var runPerd;
 var waitwave;
 
-var isbugged = false;
+var isbugged = true;
+var spMessage = "";
 //var spMessage = "Sorry, sometimes the gadget is not always loading. Please bear with the beta!";
-var spMessage = "Today's (jan 6) new API version broke voicy. Voicy disabled until I get this sorted out. Thank you for your patience.";
+//var spMessage = "Today's (jan 6) new API version broke voicy. Voicy disabled until I get this sorted out. Thank you for your patience.";
 
 function init(){
 	msg = new gadgets.MiniMessage();
